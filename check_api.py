c35f99f376dd6ef1c5dd869355bd2ad9ccea5b39
wavyboyasf <brtsz.04@gmail.com>
2026-05-06 18:52:51 +0200

    Checkc the api v2

--- check_api.py
+++ check_api.py
@@ -58,4 +58,4 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("check_api:app", host="127.0.0.1", port=8000)
